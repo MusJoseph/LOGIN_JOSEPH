41afcde546a1f7b8d3b1fcb6bce758dde062392c
MusJoseph <159422128+MusJoseph@users.noreply.github.com>
2024-02-08 11:53:41 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+function validation (email, password){
+    if (  email == "2200706408@gmail.com" || password == "2200706408@2024" )
+    console.log(`User email is ${email}, login successful`)
+    else 
+    console.log(`incorrect user credentials`)
+    }
+    
+    validation("2200706408@gmailcom", "2200706408@2024");
